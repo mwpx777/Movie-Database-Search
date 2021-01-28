7dbb35b6f2d2cf51e89646b5970757d9171a1c88
MWPX777 <mwpx777@gmail.com>
2021-01-28 11:56:34 -0600

    Clear saved searched working

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let searchHistory = document.querySelector(".searchHistory")
 let movieDate = document.querySelector("#movieDate")
 let movieInfoContainer = document.querySelector("#movieInfoContainer")
 let clearSearches = document.querySelector("#clearSearches")
-let dataCloseButton= document.querySelector('[data-close-button]')
+let dataCloseButton = document.querySelector('[data-close-button]')
 let modal = document.querySelector("#modal")
 let overlay = document.querySelector("#overlay")
 
@@ -26,9 +26,7 @@ function formSubmitHandler(event) {
         movieSearchForm.textContent = searchFormText
         //this will pass searchForm.value into movieSearch function as argument
         //  movieSearch(searchFormText);
-         movieSearch(searchFormText);
-        
-       
+        movieSearch(searchFormText);
 
         // create new <button> tag
         var savedSearchButton = document.createElement("button");
@@ -36,136 +34,176 @@ function formSubmitHandler(event) {
         savedSearchButton.classList = "savedSearch"
         // add text to the element
         savedSearchButton.textContent = searchFormText;
-        
         searchHistory.appendChild(savedSearchButton)
+
         searchArray.push(searchFormText)
-        localStorage.setItem("Saved Search", JSON.stringify (searchArray))
-        
-    
+        localStorage.setItem("Saved Search", JSON.stringify(searchArray))
+
+
 
         // this clears the movieSearchForm
         movieSearchForm.value = "";
 
 
-    } else {                          
+    } else {
         // alert("Please enter a movie");
         movieSearchForm.value = "";
         openModal();
     }
 };
 
-function openModal(){
-    console.log('openModal clicked')
-     // document.getElementById("modal").style.zIndex="10";
-    
+function openModal() {
+    // console.log('openModal clicked')    
     modal.classList.add('active');
     overlay.classList.add('active');
 
-   
 }
 
-function closeModal(){  
-    
-    console.log('closeModal clicked')
+function closeModal() {
+    // console.log('closeModal clicked')
     modal.classList.remove('active');
     overlay.classList.remove('active');
-  
-
-  
 
 }
 
-function movieSearch(keyword){
-// console.log("success");
-let baseURL = 'https://api.themoviedb.org/3/';
-let apiKey = "3e7d82fcb86e4e69fdfe0c810341a3fd";
-var movie = ''.concat(baseURL, 'search/movie?api_key=', apiKey, '&query=', keyword);
-
-// console.log(movie);
-
-
-fetch(movie).then(function (response) {
-    if (response.ok) {
-        response.json().then(function (data) {
-            displayMovieContent(data.results)
-            // console.log('fetch works');
-        });
-    }
-})
-    .catch(function (error) {
-        alert("There was a network error")
+function movieSearch(keyword) {
+    // console.log("success");
+    fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/" + keyword, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "0f5a126af5msh08292e68334f9c6p1b79aejsnddfbd878acb5",
+            "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com"
+        }
     })
+        .then(response => {
+            return (response.json());
+
+
+        }).then(response => {
+            console.log(response)
+            getMovieId(response);
+
+        })
+        .catch(err => {
+            // console.error(err);
+        });
+
 
 };
+function getMovieId(titles) {
+    console.log(titles);
+    console.log("movieid success")
+    var movieId = titles.titles[0].id  //need the movieID number here to pass into fetch 
+    console.log(movieId);
 
-function displayMovieContent (results){
-    movieInfoContainer.textContent= '';
-    var poster = results[0].poster_path
-    var movieName = results[0].title
-    var movieOverview = results[0].overview
-    var releaseDate = results[0].release_date
-    // var movieId = results[0].id
-    // console.log(movieName);
-    // console.log(movieOverview);
+
+    fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + movieId, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "0f5a126af5msh08292e68334f9c6p1b79aejsnddfbd878acb5",
+            "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com"
+        }
+    })
+        .then(response => {
+            return (response.json());
+
+        }).then(response => {
+            console.log(response)
+            displayMovieContent(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+
+function displayMovieContent(results) {
+    movieInfoContainer.textContent = '';
+    var movieName = results.title
+    var releaseDate = results.year
+    var movieLength = results.length
+    var movieOverview = results.plot
+    var poster = results.poster
+    var movieTrailer = results.trailer.link
 
 
     var movieTitleDiv = document.createElement('div');
 
     var movieTitle = document.createElement('span');
     movieTitle.classList = "movieDetails"
-    movieTitle.textContent= "Movie Title: " +  movieName
+    movieTitle.textContent = "Movie Title: " + movieName
     movieTitleDiv.appendChild(movieTitle);
     movieInfoContainer.appendChild(movieTitleDiv);
-
-    var movieSynopsisDiv = document.createElement('div');
-
-    var movieSynopsis = document.createElement('span');
-    movieSynopsis.classList= "movieDetails"
-    movieSynopsis.textContent = "Movie Overview:  " + movieOverview
-    movieSynopsisDiv.appendChild(movieSynopsis);
-    movieInfoContainer.appendChild(movieSynopsisDiv);
 
     var movieDateDiv = document.createElement('div');
 
     var movieDate = document.createElement('span');
-    movieDate.classList= "movieDetails"
+    movieDate.classList = "movieDetails"
     movieDate.textContent = "Release Date: " + releaseDate
     movieDateDiv.appendChild(movieDate)
     movieInfoContainer.appendChild(movieDateDiv);
 
+    var movieRunTimeDiv = document.createElement('div');
+
+    var movieRunTime = document.createElement('span');
+    movieRunTime.classList = "movieDetails"
+    movieRunTime.textContent = "Run Time:  " + movieLength
+    movieRunTimeDiv.appendChild(movieRunTime);
+    movieInfoContainer.appendChild(movieRunTimeDiv);
+
+
+    var movieSynopsisDiv = document.createElement('div');
+
+    var movieSynopsis = document.createElement('span');
+    movieSynopsis.classList = "movieDetails"
+    movieSynopsis.textContent = "Movie Overview:  " + movieOverview
+    movieSynopsisDiv.appendChild(movieSynopsis);
+    movieInfoContainer.appendChild(movieSynopsisDiv);
+
+
     var moviePosterDiv = document.createElement('div');
 
-    var moviePoster = document.createElement('img')     
-    moviePoster.src = "https://image.tmdb.org/t/p/w500/" + poster
+    var moviePoster = document.createElement('img')
+    moviePoster.classList = "moviePosterImg";
+    moviePoster.src = poster
     moviePosterDiv.appendChild(moviePoster);
 
     movieInfoContainer.appendChild(moviePosterDiv);
 
+    var movieTrailerDiv = document.createElement('div');
+
+    // var movieTrailerEl = document.createElement('iframe')
+    // movieTrailerEl.ref = movieTrailer
+    // movieTrailerDiv.appendChild(movieTrailerEl);
+
+    movieInfoContainer.appendChild(movieTrailerDiv);
+
     //
 }
 
-function loadSearchData(){
+function loadSearchData() {
     //if no saved data in localStorage, create new searchArray
-    if(localStorage.getItem("Saved Search") === null){
-        var searchArray = [];   
-    }else{
+    if (localStorage.getItem("Saved Search") === null) {
+        var searchArray = [];
+    } else {
         searchArray = JSON.parse(localStorage.getItem('Saved Search'))
     }
 
-    searchArray.forEach(function(stored){
-          // create new <button> tag
-          var savedSearchButton = document.createElement("button");
-          // give <div> a class
-          savedSearchButton.classList = "savedSearch"
-          // add text to the element
-          savedSearchButton.textContent = stored;
-          
-          searchHistory.appendChild(savedSearchButton)
-        
+    searchArray.forEach(function (stored) {
+        // create new <button> tag
+        var savedSearchButton = document.createElement("button");
+        // give <div> a class
+        savedSearchButton.classList = "savedSearch"
+        // add text to the element
+        savedSearchButton.textContent = stored;
+
+        searchHistory.appendChild(savedSearchButton)
+
     })
-    
+
 }
-function clearLocalStorage(){   //need to delete the buttons
+function clearLocalStorage() {   //need to delete the buttons
+    searchHistory.innerHTML="";
     localStorage.clear()
 }
 
@@ -173,19 +211,17 @@ function clearLocalStorage(){   //need to delete the buttons
 //variable for sending searchHistory to savedSearches function
 $('.searchHistory').on('click', 'button', function () {
     var savedSearchButton = $(this).text();
-    // console.log('saved button')
-    // currentCityName.innerHTML = "";
-    // weatherIcon.innerHTML = "",
+
     savedSearches(savedSearchButton);
     // console.log(savedSearchButton);
-    
+
 });
 
-function savedSearches(savedButton){
+function savedSearches(savedButton) {
     // console.log(savedButton)
     // console.log('button clicked')
     movieSearch(savedButton);
-    
+
 }
 
 searchButton.addEventListener('click', formSubmitHandler)
